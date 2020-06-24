7cacf5ccbb624fe1252127472c48afe8b5d455cf
Tushar Kapila <tgkprog@gmail.com>
2020-06-25 03:02:40 +0530

    styling & get params n and t

---
+++ site1/index.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html lang="en"><head>
+
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<title>sel2in HTML Programming examples</title>
+<style>
+body{border:10px;line-height: 130%;
+
+background-color: #45a1a1;
+}
+</style>
+</head>
+
+<body>
+
+<h3>Tools and sample</h3>
+
+<a href=tapToMsgMakeUrl.html>Enter number get URL to tap, to whats app 
+ message that person/ business.</><br>
+
+
+ <a href=imageChange.html title='iframe sample'>Image change </a>
+<br>
+
+
+AJAX SAMPLES
+<br>
+<a href=ajax title='ajax sample'>a</a>  |
+<br>
+<a href=owes.html>Who owes who</a><br>
+
+<a href=FileUploadShellOnlyStart.php>File Upload Test</a><br>
+
+<a href='html/spell-check.html' title='Browser based spell checker- text area'>Spell check</a>
+
+<p>Html JavaScript Utilities and example code <a href=html>html js</a></p>
+
+<br>
+Tushar Kapila 2006-2020 <a href=http://sel2in.com/>home</a>
